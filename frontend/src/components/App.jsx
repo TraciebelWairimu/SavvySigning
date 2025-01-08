9babb96965cc33b21c6021f1a531c41a8902fe8f
Traciebel Kinyari <traciebel.kinyari@strathmore.edu>
2025-01-08 22:30:46 +0300

    Add routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,6 +9,7 @@ import ProtectedRoute from "./ProtectedRoute.jsx"
 import Landing from "../pages/Landing.jsx";
 import Translate from "../pages/Translate.jsx";
 import Signs from "../pages/Signs.jsx";
+import Instructions from "../pages/Instructions.jsx";
 
 function Logout() {
   localStorage.clear()
@@ -22,25 +23,26 @@ function RegisterAndLogout() {
 
 function App() {
   return (
-      <div className="App">
-         <Routes>
-           <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<RegisterAndLogout />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/landing" element={<Landing />} />
-          <Route path="/translate" element={<Translate />} />
-          <Route path="/signs" element={<Signs />} />
-        </Routes>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/translate" element={<Translate />} />
+        <Route path="/signs" element={<Signs />} />
+        <Route path="/instructions" element={<Instructions />} />
+      </Routes>
+    </div>
   )
 }
 
