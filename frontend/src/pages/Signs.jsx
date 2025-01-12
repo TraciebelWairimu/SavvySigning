c2421bbde3e1ceb4a6f9005ab3a30dbba938cc79
Maureen Njihia <maureennjhia468@gmail.com>
2025-01-12 19:58:33 +0300

    Complete signs page

--- frontend/src/pages/Signs.jsx
+++ frontend/src/pages/Signs.jsx
@@ -1,14 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from "../components/Header.jsx";
+import Signs1 from "../../../Images/Alphabet_signs-removebg-preview.png"
+import Signs2 from "../../../Images/more signs.jpg"
 
 const Signs = () => {
     return (
         <>
-            <div className="min-h-screen bg-white relative">
-                <Header />
-                <div className="relative min-h-[100vh] flex flex-col">
-                    <div className="absolute inset-0 bg-landingBg bg-no-repeat bg-cover opacity-50 z-0"></div>
+            <div className="min-h-screen bg-white ">
+                <Header/>
+                <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4 p-8">
+                    <div>
+                        <img src={Signs1} alt="Signs1" className="mx-auto lg:mx-0 w-full"/>
+                    </div>
+
+                    <div>
+                        <img src={Signs2} alt="Signs2" className="mx-auto lg:mx-0 w-full"/>
+                    </div>
                 </div>
             </div>
         </>
